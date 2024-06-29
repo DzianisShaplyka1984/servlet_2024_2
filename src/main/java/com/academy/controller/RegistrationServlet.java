34847134b507cc81533c7da30ad179759944e85f
Dzianis_Shaplyka <DShaplyka@inspirebrands.com>
2024-06-29 09:54:23 +0300

    Update registration servlet. Move view to jsp

--- src/main/java/com/academy/controller/RegistrationServlet.java
+++ src/main/java/com/academy/controller/RegistrationServlet.java
@@ -1,27 +1,15 @@
 package com.academy.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class RegistrationServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/html");
-
-        PrintWriter printWriter = resp.getWriter();
-
-        printWriter.println("<html>");
-        printWriter.println("<head>");
-        printWriter.println("</head>");
-        printWriter.println("<body>");
-        printWriter.println("<h1>Registration success</h1>");
-        printWriter.println("</body>");
-        printWriter.println("</html>");
-
-        printWriter.close();
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        req.getRequestDispatcher("/WEB-INF/jsp/reistration.jsp").forward(req, resp);
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Dzianis_Shaplyka
+  Date: 6/29/2024
+  Time: 9:52 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8"%>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Registration success</h1>
+</body>
+</html>
