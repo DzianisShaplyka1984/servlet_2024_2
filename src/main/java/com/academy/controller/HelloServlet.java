fbded6a1b6f07bbee537998c07fc932b04e8ffdc
Dzianis_Shaplyka <DShaplyka@inspirebrands.com>
2024-06-29 10:05:20 +0300

    Update HelloServlet. Update msg in Session.

--- src/main/java/com/academy/controller/HelloServlet.java
+++ src/main/java/com/academy/controller/HelloServlet.java
@@ -4,24 +4,26 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
 
-        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request,
+                                                                       response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) {
         HttpSession session = request.getSession();
 
-        session.setAttribute("msg", "Hello World!");
+        session.setAttribute("msg",
+                             "Hello World!!!");
     }
 }
